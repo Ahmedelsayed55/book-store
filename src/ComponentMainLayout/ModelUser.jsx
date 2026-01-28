76103e7148ebeaa3985e25686ebfa9b717aed09d
Ahmedelsayed55 <am1096922@gmail.com>
2026-01-28 17:01:48 +0200

    handel log out

--- src/ComponentMainLayout/ModelUser.jsx
+++ src/ComponentMainLayout/ModelUser.jsx
@@ -3,9 +3,16 @@ import { AiOutlineExclamationCircle } from 'react-icons/ai'
 import { CiUser } from 'react-icons/ci'
 import { IoLocationOutline, IoTimeOutline } from 'react-icons/io5'
 import { RxExit } from 'react-icons/rx'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const ModelUser = () => {
+  const navigate = useNavigate()
+  const handleLogout = () => {
+  localStorage.removeItem("userInfo");
+  localStorage.removeItem("token");
+
+  navigate("/home/login");
+};
   return (
     <div className='w-full md:w-70 absolute top-20 text-black left-1/2 -translate-x-1/2  '>
         <h3 className='text-[14px] py-2'>Profile</h3>
@@ -13,18 +20,18 @@ const ModelUser = () => {
             <Link className='w-full flex items-center gap-3 p-4 rounded duration-300 hover:bg-gray-200 ' to={"profile"}>
               <CiUser />  Profile
             </Link>
-            <Link className='w-full flex items-center gap-3 p-4 rounded duration-300 hover:bg-gray-200 ' to={"profile"}>
+            <Link className='w-full flex items-center gap-3 p-4 rounded duration-300 hover:bg-gray-200 ' to={"order"}>
               <IoTimeOutline />  Order History
             </Link>
             <Link className='w-full flex items-center gap-3 p-4 rounded duration-300 hover:bg-gray-200 ' to={"profile"}>
               <IoLocationOutline />  Address
             </Link>
-            <Link className='w-full flex items-center gap-3 p-4 rounded duration-300 hover:bg-gray-200 ' to={"profile"}>
+            <Link className='w-full flex items-center gap-3 p-4 rounded duration-300 hover:bg-gray-200 ' to={"help"}>
               <AiOutlineExclamationCircle />  Help
             </Link>
-            <Link className='w-full flex items-center gap-3 p-4 rounded duration-300 hover:bg-gray-200 ' to={"profile"}>
+            <p className='w-full flex items-center gap-3 p-4 rounded duration-300 hover:bg-gray-200 ' onClick={handleLogout} >
               <RxExit />  Log Out
-            </Link>
+            </p>
 
         </div>
       
